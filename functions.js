19fe53f5d5d9c04f0c1d67430ab1a234e89f7938
Vicente Canales <vicente.canales@automattic.com>
2023-10-15 18:42:36 -0300

    lib: add status command

--- functions.js
+++ functions.js
@@ -1,21 +1,22 @@
 import { exec, spawn } from 'child_process';
 import { readFileSync, writeFileSync } from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
 
-const { HOST, PORT, KEY_PATH, USER } = process.env;
-const controlSocketName = `ssh-socks-proxy-${USER}-${HOST}-${PORT}`;
-const CONTROL_SOCKET = path.resolve('socks', controlSocketName);
+dotenv.config();
 
-function start() {
+const { PROXY_HOST, PROXY_PORT, KEY_PATH, PROXY_USER } = process.env;
+
+export function start() {
 	// use spawn to run the ssh command above
 	const ssh = spawn('ssh', [
-		'-NvD', '8080',
-		'-M', '-S', CONTROL_SOCKET,
+		'-NvD', PROXY_PORT,
+		'-M', '-S', 'ssh-socks-proxy-8080',
 		'-fnT', '-i', KEY_PATH,
-		'-p', PORT,
+		'-p', '22',
 		'-vvv',
 		'-E', 'ssh.log',
-		`${USER}@${HOST}`
+		`${PROXY_USER}@${PROXY_HOST}`
 	], { detached: true });
 
 	writeFileSync('pid', `${ssh.pid}`);
@@ -23,17 +24,64 @@ function start() {
 	ssh.unref();
 }
 
-function stop() {
+export function stop() {
 	const pid = readFileSync('pid', 'utf8');
 	exec(`kill ${pid}`);
 	// delete pid file
 	exec('rm pid');
 }
 
-export {
-	start,
-	stop,
-};
+export function status() {
+	const pidFile = readFileSync('pid', 'utf8');
+
+	if (!pidFile) {
+		return false;
+	}
+
+	const ssh = spawn('ssh', [
+		'-O', 'check',
+		'-S', 'ssh-socks-proxy-8080',
+		'-i', KEY_PATH,
+		'-p', PROXY_PORT,
+		`${PROXY_USER}@${PROXY_HOST}`
+	]);
+
+	ssh.stdout.on('data', data => {
+		console.log('data', data.toString());
+	});
+
+	ssh.stderr.on('data', data => {
+		console.log('data', data.toString());
+	});
+
+	ssh.on('close', code => {
+		console.log('code', code);
+	});
+
+	ssh.on('error', error => {
+		console.log('error', error);
+	});
+
+	ssh.on('exit', code => {
+		console.log('exit', code);
+	});
+
+	ssh.on('disconnect', () => {
+		console.log('disconnect');
+	});
+
+	ssh.on('message', message => {
+		console.log('message', message);
+	});
+
+	ssh.on('spawn', () => {
+		console.log('spawn');
+	});
+
+	ssh.on('disconnect', () => {
+		console.log('disconnect');
+	});
+}
 
 
 
