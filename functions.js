6468afeec57c1bb20c61aeef39ec78a2f13b546f
Vicente Canales <vicente.canales@automattic.com>
2023-10-15 12:40:36 -0300

    lib: add execution permissions

--- functions.js
+++ functions.js
@@ -2,28 +2,21 @@ import { exec, spawn } from 'child_process';
 import { readFileSync, writeFileSync } from 'fs';
 import path from 'path';
 
-const config = {
-	key: {
-		path: '/Users/vcanales/.ssh/id_ed25519',
-	}
-};
-
-const HOST = 'vforvicente@proxy.automattic.com';
-const PORT = '22';
-const KEY = config.key.path;
-const CONTROL_SOCKET = path.resolve('socks','ssh-socks-proxy-8080');
+const { HOST, PORT, KEY_PATH, USER } = process.env;
+const controlSocketName = `ssh-socks-proxy-${USER}-${HOST}-${PORT}`;
+const CONTROL_SOCKET = path.resolve('socks', controlSocketName);
 
 function start() {
 	// use spawn to run the ssh command above
 	const ssh = spawn('ssh', [
 		'-NvD', '8080',
 		'-M', '-S', CONTROL_SOCKET,
-		'-fnT', '-i', KEY,
+		'-fnT', '-i', KEY_PATH,
 		'-o', 'UserKnownHostsFile="/Applications/AutoProxxy.app/Contents/Resources/ProxyHostKey.pub"',
 		'-p', PORT,
 		'-vvv',
 		'-E', 'ssh.log',
-		HOST
+		`${USER}@${HOST}`
 	], { detached: true });
 
 	writeFileSync('pid', `${ssh.pid}`);
