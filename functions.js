2d79b5aed5003b6fcf85b547e028c7375c29e100
Vicente Canales <vicente.canales@automattic.com>
2023-10-16 10:40:24 -0300

    lib: move messaging to main file

--- functions.js
+++ functions.js
@@ -1,87 +1,85 @@
-import { exec, spawn } from 'child_process';
-import { readFileSync, writeFileSync } from 'fs';
+import { execSync, spawn, spawnSync } from 'child_process';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const { PROXY_HOST, PROXY_PORT, KEY_PATH, PROXY_USER } = process.env;
+const PROXY_KEY = path.join(__dirname, 'ProxyHostKey.pub');
+const PID_FILE = path.join(__dirname, 'pid');
+const SSH_LOG = path.join(__dirname, 'ssh.log');
+const SSH_SOCKS_PROXY = path.join(__dirname, `ssh-socks-proxy-${PROXY_USER}-${PROXY_PORT}`);
 
 export function start() {
+	const ss = status();
+	
+	if (ss) {
+		console.log('ssh proxy is already running');
+		return;
+	}
+
 	// use spawn to run the ssh command above
 	const ssh = spawn('ssh', [
 		'-NvD', PROXY_PORT,
-		'-M', '-S', 'ssh-socks-proxy-8080',
+		'-M', '-S', SSH_SOCKS_PROXY,
 		'-fnT', '-i', KEY_PATH,
-		'-o', 'UserKnownHostsFile="/Applications/AutoProxxy.app/Contents/Resources/ProxyHostKey.pub"',
+		'-o', `UserKnownHostsFile="${PROXY_KEY}"`,
 		'-p', '22',
 		'-vvv',
-		'-E', 'ssh.log',
+		'-E', SSH_LOG,
 		`${PROXY_USER}@${PROXY_HOST}`
 	], { detached: true });
 
-	writeFileSync('pid', `${ssh.pid}`);
+	writeFileSync(PID_FILE, `${ssh.pid}`);
 
 	ssh.unref();
 }
 
 export function stop() {
-	const pid = readFileSync('pid', 'utf8');
-	exec(`kill ${pid}`);
-	// delete pid file
-	exec('rm pid');
+	const pid = status(true);
+	
+	if (!pid) {
+		console.log('ssh proxy is not running');
+		return;
+	}
+	
+	try {
+		execSync(`kill ${pid}`);
+	} catch(err) {
+		console.log('ssh process is not found.');
+		return;
+	}
 }
 
-export function status() {
-	const pidFile = readFileSync('pid', 'utf8');
+export function status(returnPid = false) {
+	const pidFile = existsSync(PID_FILE);
+	const socksProxy = existsSync(SSH_SOCKS_PROXY);
 
-	if (!pidFile) {
+	if (!pidFile || !socksProxy) {
 		return false;
 	}
 
-	const ssh = spawn('ssh', [
+	const ssh = spawnSync('ssh', [
 		'-O', 'check',
-		'-S', 'ssh-socks-proxy-8080',
-		'-i', KEY_PATH,
-		'-p', PROXY_PORT,
+		'-S', SSH_SOCKS_PROXY,
 		`${PROXY_USER}@${PROXY_HOST}`
 	]);
 
-	ssh.stdout.on('data', data => {
-		console.log('data', data.toString());
-	});
+	const { stderr } = ssh;
 
-	ssh.stderr.on('data', data => {
-		console.log('data', data.toString());
-	});
+	if (stderr.includes('Master running')) {
+		if (returnPid) {
+			const pid = stderr.toString().match(/pid=(\d+)/)[1];
+			return pid;
+		}
+		return true;
+	}
 
-	ssh.on('close', code => {
-		console.log('code', code);
-	});
-
-	ssh.on('error', error => {
-		console.log('error', error);
-	});
-
-	ssh.on('exit', code => {
-		console.log('exit', code);
-	});
-
-	ssh.on('disconnect', () => {
-		console.log('disconnect');
-	});
-
-	ssh.on('message', message => {
-		console.log('message', message);
-	});
-
-	ssh.on('spawn', () => {
-		console.log('spawn');
-	});
-
-	ssh.on('disconnect', () => {
-		console.log('disconnect');
-	});
+	return false;
 }
 
 
